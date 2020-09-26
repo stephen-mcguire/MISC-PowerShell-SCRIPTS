4c1672bdb411023e53aae6ed2a3621848d84d8c3
Stephen McGuire <50688225+stephen-mcguire@users.noreply.github.com>
2020-09-26 03:37:28 -0400

    Update Remove-Hosts - Copy.ps1

--- Remove-Hosts - Copy.ps1
+++ Remove-Hosts - Copy.ps1
@@ -1,4 +1,4 @@
-$connect = Connect-VIServer "psvcpw01.jewelry.acn"
+$connect = Connect-VIServer "SERVERNAME_HERE"
 $hostEntries = "10.10.10.10[\s+](domain.here.for.reference|another.domain.here.for.reference|proxy.here.for.reference|email.server.for.reference)"
 $hostsPath = "\c$\Windows\System32\drivers\etc\hosts"
 $vms = Get-VM -Location * | Where-Object { $_.ExtensionData.Guest.GuestFullName -match "Windows 7" -and $_.Name -like "V[0-9]*" } #For the real thing
