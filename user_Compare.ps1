739e3de65b0bcb5fb5abf57119a0ee3c3dfe4197
Stephen McGuire <50688225+stephen-mcguire@users.noreply.github.com>
2024-09-12 23:29:26 -0400

    User Compare Permissions

---
+++ user_Compare.ps1
@@ -0,0 +1,9 @@
+﻿<#
+This script compares the group memberships of two AD users (newUser and existingUser) 
+and outputs the groups that are different between them, sorted alphabetically by group name.
+#>
+import-module activedirectory
+#Get the user you are wanting to compare access
+$newUser = Read-Host 'What is the name of the user you want to compare?'
+$existingUser = Read-Host 'What is the name of the existing user you are wanting to compare?'
+Compare-Object -ReferenceObject (Get-AdPrincipalGroupMembership $newUser | select name | sort-object -Property name) -DifferenceObject (Get-AdPrincipalGroupMembership $existingUser | select name | sort-object -Property name) -property name -passthru
